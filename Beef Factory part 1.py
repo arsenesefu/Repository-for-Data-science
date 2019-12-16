19c125442fd8ad85fd8bbda7c230441e9f0ea686
arsenesefu <57950496+arsenesefu@users.noreply.github.com>
2019-12-16 19:49:17 +0100

    Add files via upload

---
+++ Beef Factory part 1.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# Introduction
+# 
+
+# Beef Factory is a company based in Paris for selling burgers and sandwiches, currently it works on a project to launch a shop in Brussels 
+
+# Data Description
+# 
+# 
+
+# The data used in this project is provided by Foursquare location data. The data are grouped by landscape area, and each area included the information about this area and all information about restaurants, cafes, and stores which in this area.
+
+# Table of contents : Import Libraries ; Define Foursquare Credentials ; Define the city and get its latitude & longitude ; Search for Universities & clean dataframe ; Search for Parks & clean dataframe ; Search for Restaurants & clean dataframe ; Search for Cafeteria & clean dataframe ; Search for Shopping Stores & clean dataframe ; Generate map to visualize hotels, shopping stores and Cafeteria and how they cluster together and Generate map to visualize Park, Restaurant and Cafeteria and how they cluster together.
+
+# In[ ]:
+
+
+
+
